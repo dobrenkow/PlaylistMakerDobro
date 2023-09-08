d87a1a7a3005a1e4a81317f62326f9df0b28b409
fedor <dobrenkow2012@yandex.ru>
2023-09-08 13:58:13 +0300

    3-commit

---
+++ src/main/java/com/example/playlistmakerdobro/LibraryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmakerdobro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
--- src/main/java/com/example/playlistmakerdobro/MainActivity.kt
+++ src/main/java/com/example/playlistmakerdobro/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.playlistmakerdobro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        libraryButton.setOnClickListener {
+            val libraryIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libraryIntent)
+        }
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+
     }
 }
